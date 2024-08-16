c332eeb905c73827eed5160317fbab5c8e010dd9
Randy J. Ray <rjray@blackperl.com>
2024-08-16 16:51:59 -0700

    Adjustment to rules-set

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -2,8 +2,9 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict
 );
