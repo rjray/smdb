096ae7694300e1b751dd812cb0a9b63216ff0003
Randy J. Ray <rjray@blackperl.com>
2024-09-13 09:45:29 -0700

    Add typing

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -2,7 +2,7 @@
  * Constants used in the application.
  */
 
-export const ReferenceTypes = {
+export const ReferenceTypes: Record<string, number> = {
   Book: 1,
   MagazineFeature: 2,
   PhotoCollection: 3,
