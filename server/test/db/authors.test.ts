b8309f990e3878698fd96d05ddabf34852a2a8bf
Randy J. Ray <rjray@blackperl.com>
2024-09-12 20:03:46 -0700

    Make work with in-memory db, add tests

--- server/test/db/authors.test.ts
+++ server/test/db/authors.test.ts
@@ -1,16 +1,22 @@
-import { afterAll, beforeAll, describe, expect, test } from "vitest";
+/**
+ * Test suite for the database code in the `db/authors.ts` module.
+ */
+
+import { afterAll, beforeAll, describe, expect, test, assert } from "vitest";
 import fs from "fs";
 
 import setupDatabase from "database/setup";
 import { Authors } from "db";
 
-const file = process.env.DATABASE_FILE || "test.db";
+// Need to have this here in case the test file is an actual file rather than
+// an in-memory database.
+const file = process.env.DATABASE_FILE || ":memory:";
 
 beforeAll(async () => {
   if (fs.existsSync(file)) {
     fs.unlinkSync(file);
   }
-  await setupDatabase(file, "src/database");
+  await setupDatabase("src");
 });
 afterAll(async () => {
   if (fs.existsSync(file)) {
@@ -27,5 +33,47 @@ describe("Authors: Creation", () => {
     expect(author.name).toBe("Author 1");
     expect(author.aliases).toBeUndefined();
     expect(author.references).toBeUndefined();
+  });
+
+  test("Create an author with aliases", async () => {
+    const author = await Authors.addAuthor({
+      name: "Author 2",
+      aliases: [{ name: "Alias 1" }, { name: "Alias 2" }],
+    });
+
+    expect(author.name).toBe("Author 2");
+    if (author.aliases) {
+      expect(author.aliases.length).toBe(2);
+      expect(author.aliases[0].name).toBe("Alias 1");
+      expect(author.aliases[1].name).toBe("Alias 2");
+    } else {
+      assert.fail("No author.aliases found");
+    }
+    expect(author.references).toBeUndefined();
+  });
+});
+
+describe("Authors: Retrieval", () => {
+  test("Get all authors", async () => {
+    const authors = await Authors.fetchAllAuthors();
+    expect(authors.length).toBe(2);
+  });
+
+  test("Get author by ID", async () => {
+    const author = await Authors.fetchOneAuthor(1);
+    expect(author?.id).toBe(1);
+    expect(author?.name).toBe("Author 1");
+  });
+
+  test("Get author with aliases by ID", async () => {
+    const author = await Authors.fetchOneAuthor(2, { aliases: true });
+    expect(author?.id).toBe(2);
+    if (author?.aliases) {
+      expect(author.aliases.length).toBe(2);
+      expect(author.aliases[0].name).toBe("Alias 1");
+      expect(author.aliases[1].name).toBe("Alias 2");
+    } else {
+      assert.fail("No author.aliases found");
+    }
   });
 });
