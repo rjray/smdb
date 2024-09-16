cfbf7227425eefe5a86ec121a42062c5bff6938b
Randy J. Ray <rjray@blackperl.com>
2024-09-16 16:50:25 -0700

    Test suite for db/featuretags.ts

---
+++ server/test/db/featuretags.test.ts
@@ -0,0 +1,150 @@
+/**
+ * Test suite for the database code in the `db/featuretags.ts` module.
+ */
+
+import { afterAll, beforeAll, describe, expect, test, assert } from "vitest";
+import fs from "fs";
+
+import setupDatabase from "database/setup";
+import { FeatureTags, Magazines, MagazineIssues, References } from "db";
+// Need a full relative path due to deprecated "constants" module in Node.
+import { ReferenceTypes } from "../../src/constants";
+
+// Need to have this here in case the test file is an actual file rather than
+// an in-memory database.
+const file = process.env.DATABASE_FILE || ":memory:";
+
+beforeAll(async () => {
+  if (fs.existsSync(file)) {
+    fs.unlinkSync(file);
+  }
+  await setupDatabase("src");
+
+  // Create some magazines and issues here, before tests run. This way, if only
+  // a single test runs, it will still have the data on hand.
+  for (const magazineId of [1, 2, 3, 4, 5]) {
+    await Magazines.createMagazine({
+      name: `Magazine ${magazineId}`,
+      id: magazineId,
+    });
+
+    for (const issueId of [1, 2, 3, 4, 5]) {
+      const issue = await MagazineIssues.createMagazineIssue({
+        magazineId,
+        issue: String(issueId),
+      });
+
+      await References.createReference({
+        name: `Magazine Feature ${magazineId}`,
+        referenceTypeId: ReferenceTypes.MagazineFeature,
+        tags: [{ id: 1 }, { id: 2 }],
+        magazineFeature: {
+          magazineId,
+          magazineIssueId: issue.id,
+          featureTags: [{ id: issueId }, { id: issueId + 1 }],
+        },
+      });
+    }
+  }
+});
+afterAll(async () => {
+  if (fs.existsSync(file)) {
+    fs.unlinkSync(file);
+  }
+});
+
+describe("FeatureTags: Create", () => {
+  test("Create basic feature tag", async () => {
+    const featureTag = await FeatureTags.createFeatureTag({
+      name: "Feature 1",
+    });
+
+    expect(featureTag.id).toBe(30);
+    expect(featureTag.name).toBe("Feature 1");
+  });
+
+  test("Create feature tag with conflicting name", async () => {
+    async function failToCreate() {
+      return await FeatureTags.createFeatureTag({
+        name: "Feature 1",
+      });
+    }
+
+    expect(() => failToCreate()).rejects.toThrowError("Validation error");
+  });
+});
+
+describe("FeatureTags: Retrieve", () => {
+  test("Get all feature tags", async () => {
+    const featureTags = await FeatureTags.getAllFeatureTags();
+
+    expect(featureTags.length).toBe(30);
+    expect(featureTags[0].id).toBe(1);
+    expect(featureTags[0].name).toBe("color illustrations");
+  });
+
+  test("Get all feature tags with reference counts", async () => {
+    const featureTags = await FeatureTags.getAllFeatureTags({
+      referenceCount: true,
+    });
+
+    const counts = [5, 10, 10, 10, 10, 5];
+    const withRefs = featureTags.slice(0, 6);
+    withRefs.forEach((featureTag, i) => {
+      expect(featureTag.referenceCount).toBe(counts[i]);
+    });
+  });
+
+  test("Get feature tag by ID", async () => {
+    const featureTag = await FeatureTags.getFeatureTagById(1);
+
+    if (featureTag) {
+      expect(featureTag.id).toBe(1);
+      expect(featureTag.name).toBe("color illustrations");
+    } else {
+      assert.fail("No feature tag found");
+    }
+  });
+
+  test("Get feature tag by ID with references", async () => {
+    const featureTag = await FeatureTags.getFeatureTagById(1, {
+      features: true,
+    });
+
+    if (featureTag) {
+      expect(featureTag.id).toBe(1);
+      expect(featureTag.name).toBe("color illustrations");
+      if (featureTag.magazineFeatures) {
+        expect(featureTag.magazineFeatures.length).toBe(5);
+      } else {
+        assert.fail("No feature tag.references found");
+      }
+    } else {
+      assert.fail("No feature tag found");
+    }
+  });
+});
+
+describe("FeatureTags: Update", () => {
+  test("Update feature tag", async () => {
+    const featureTag = await FeatureTags.getFeatureTagById(1);
+    if (featureTag) {
+      expect(featureTag.id).toBe(1);
+      expect(featureTag.name).toBe("color illustrations");
+      const result = await FeatureTags.updateFeatureTagById(1, {
+        name: "color illustrations updated",
+      });
+      expect(result.id).toBe(1);
+      expect(result.name).toBe("color illustrations updated");
+    } else {
+      assert.fail("No feature tag found");
+    }
+  });
+});
+
+describe("FeatureTags: Delete", () => {
+  test("Delete feature tag", async () => {
+    const result = await FeatureTags.deleteFeatureTagById(1);
+    expect(result).toBe(1);
+  });
+});
