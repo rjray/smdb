60e4e17ded8836fa045054f3aa322f61b27171ca
Randy J. Ray <rjray@blackperl.com>
2025-07-13 12:59:46 -0700

    Basic checkpoint for MUI/Vite client
    Most of this will be overwritten or outright replaced.

---
+++ client/src/main.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
